90129d92dfc415bb1dd305cc14055ff5a6ab3e60
yyforyongyu <yong2452@gmail.com>
2024-06-05 02:20:21 +0800

    chainio: add `Blockbeat` and `BlockbeatDispatcher`
    In this commit, a minimal version of `Blockbeat` is added to synchronize
    block heights, which will be used in `ChainArb`, `Sweeper`, and
    `TxPublisher` so blocks are processed sequentially among them.

--- chainio/blockbeat.go
+++ chainio/blockbeat.go
@@ -2,11 +2,17 @@ package chainio
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/btcsuite/btclog"
 	"github.com/lightningnetwork/lnd/build"
 	"github.com/lightningnetwork/lnd/chainntnfs"
+	"github.com/lightningnetwork/lnd/fn"
 )
+
+// DefaultProcessBlockTimeout is the timeout value used when waiting for one
+// consumer to finish processing the new block epoch.
+var DefaultProcessBlockTimeout = 30 * time.Second
 
 // Blockbeat contains the block epoch and a buffer error chan.
 //
@@ -39,4 +45,86 @@ func NewBlockbeat(epoch chainntnfs.BlockEpoch) Blockbeat {
 	b.log = build.NewPrefixLog(logPrefix, clog)
 
 	return b
+}
+
+// notifyAndWait sends the blockbeat to the specified consumer. It requires the
+// consumer to finish processing the block under 30s, otherwise a timeout error
+// is returned.
+func notifyAndWait(c Consumer, beat Blockbeat) error {
+	// Construct a new beat with a buffered error chan.
+	b := NewBlockbeat(beat.Epoch)
+
+	b.log.Debugf("Notifying block and wait for consumer=%v to process it",
+		c.Name())
+
+	// Record the time it takes the consumer to process this block.
+	start := time.Now()
+
+	// We expect the consumer to finish processing this block under 30s,
+	// otherwise a timeout error is returned.
+	err, timeout := fn.RecvOrTimeout(
+		c.ProcessBlock(b), DefaultProcessBlockTimeout,
+	)
+	if err != nil {
+		return fmt.Errorf("%s: ProcessBlock got: %w", c.Name(), err)
+	}
+	if timeout != nil {
+		return fmt.Errorf("%s timed out while processing block",
+			c.Name())
+	}
+
+	b.log.Debugf("Consumer [%s] processed block in %v", c.Name(),
+		time.Since(start))
+
+	return nil
+}
+
+// NotifySequential takes a list of consumers and notify them about the new
+// epoch sequentially.
+func NotifySequential(consumers []Consumer, b Blockbeat) error {
+	for _, c := range consumers {
+		// Send the copy of the beat to the consumer.
+		if err := notifyAndWait(c, b); err != nil {
+			b.log.Errorf("Consumer=%v failed to process "+
+				"block: %v", c.Name(), err)
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+// NotifyConcurrent notifies each consumer concurrently about the latest block
+// epoch.
+func NotifyConcurrent(consumers []Consumer, b Blockbeat) error {
+	// errChans is a map of channels that will be used to receive errors
+	// returned from notifying the consumers.
+	errChans := make(map[string]chan error, len(consumers))
+
+	// Notify each queue in goroutines.
+	for _, c := range consumers {
+		// Create a signal chan.
+		errChan := make(chan error, 1)
+		errChans[c.Name()] = errChan
+
+		// Notify each consumer concurrently.
+		go func(c Consumer, b Blockbeat) {
+			// Send the copy of the beat to the consumer.
+			errChan <- notifyAndWait(c, b)
+		}(c, b)
+	}
+
+	// Wait for all consumers in each queue to finish.
+	for name, errChan := range errChans {
+		err := <-errChan
+		if err != nil {
+			b.log.Errorf("Consumer=%v failed to process block: %v",
+				name, err)
+
+			return err
+		}
+	}
+
+	return nil
 }
