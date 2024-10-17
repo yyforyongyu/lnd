7daaa120edde2c1196a6bffec7805e62018bf770
yyforyongyu <yong2452@gmail.com>
2024-10-18 05:35:33 +0800

    x - delete

--- lntest/node/config.go
+++ lntest/node/config.go
@@ -233,7 +233,7 @@ func (cfg *BaseNodeConfig) GenArgs() []string {
 
 	nodeArgs := []string{
 		"--nobootstrap",
-		"--debuglevel=debug",
+		"--debuglevel=info,SWPR=debug,CHIO=trace,CNCT=debug",
 		"--bitcoin.defaultchanconfs=1",
 		"--accept-keysend",
 		"--keep-failed-payment-attempts",
