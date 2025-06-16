dbfac347ed6c17d83d42f5139906ccbf3b8ea970
yyforyongyu <yong2452@gmail.com>
2025-06-16 19:28:51 +0800

    temp: add new itest for upgrading chan type

--- itest/lnd_dyn_test.go
+++ itest/lnd_dyn_test.go
@@ -66,3 +66,63 @@ func testDynUpgradeChanParams(ht *lntest.HarnessTest) {
 		}
 	}
 }
+
+func testDynUpgradeChanType(ht *lntest.HarnessTest) {
+	// Prepare params.
+	cfg := []string{
+		"--protocol.anchors",
+		// TODO: remove this once upgrader has its own msg flow.
+		"--channel-commit-interval=1m",
+		"--protocol.simple-taproot-chans",
+	}
+	cfgs := [][]string{cfg, cfg}
+
+	openChannelParams := lntest.OpenChannelParams{
+		Amt:            invoiceAmt * 10,
+		Private:        true,
+		CommitmentType: lnrpc.CommitmentType_ANCHORS,
+	}
+	chanPoints, nodes := ht.CreateSimpleNetwork(cfgs, openChannelParams)
+	cp := chanPoints[0]
+	alice, bob := nodes[0], nodes[1]
+
+	bob.RPC.GetInfo()
+
+	newCommitType := lnrpc.CommitmentType_SIMPLE_TAPROOT
+
+	req := &lnrpc.UpgradeChannelRequest{
+		ChannelPoint:   cp,
+		CommitmentType: &newCommitType,
+	}
+	stream, err := alice.RPC.LN.UpgradeChannel(ht.Context(), req)
+	require.NoError(ht, err)
+
+	respChan := make(chan *lnrpc.UpgradeChannelResponse, 1)
+	errChan := make(chan error)
+	go func() {
+		for {
+			// Consume one message. This will block until the
+			// message is received.
+			resp, err := stream.Recv()
+			if err != nil {
+				errChan <- err
+				return
+			}
+			respChan <- resp
+		}
+	}()
+
+	for {
+		select {
+		case <-time.After(time.Second * 5):
+			ht.Logf("==========> timeout")
+			return
+
+		case err := <-errChan:
+			ht.Logf("Received err %v", err)
+
+		case resp := <-respChan:
+			ht.Logf("Received resp %v", resp)
+		}
+	}
+}
