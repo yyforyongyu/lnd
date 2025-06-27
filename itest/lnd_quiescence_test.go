b2e3d9d2a863dc35b53a302cbc97015100a40d9f
yyforyongyu <yong2452@gmail.com>
2025-06-27 21:13:09 +0800

    itest+lncfg: test the quiescence timeout behaivor

--- itest/lnd_quiescence_test.go
+++ itest/lnd_quiescence_test.go
@@ -1,6 +1,8 @@
 package itest
 
 import (
+	"time"
+
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/devrpc"
@@ -60,4 +62,44 @@ func testQuiescence(ht *lntest.HarnessTest) {
 		alice, req,
 		lnrpc.PaymentFailureReason_FAILURE_REASON_INSUFFICIENT_BALANCE,
 	)
+
+	// Alice now restarts with an extremely short quiescence timeout.
+	ht.RestartNodeWithExtraArgs(
+		alice, []string{"--htlcswitch.quiescencetimeout=1s"},
+	)
+
+	// Once restarted, the channel is no longer quiescent so Alice can
+	// finish the payment for invoice2.
+	ht.SendPaymentAssertSettled(alice, req)
+
+	// Bob adds another invoice.
+	invoice3 := bob.RPC.AddInvoice(invReq)
+
+	// Alice now requires the channel to be quiescent again. Since we are
+	// using a short timeout (1ms) for the quiescence, Alice should
+	// disconnect from Bob immediately.
+	res = alice.RPC.Quiesce(&devrpc.QuiescenceRequest{
+		ChanId: chanPoint,
+	})
+	require.True(ht, res.Initiator)
+
+	// Sleep 2 seconds - we expect the above quiescence to timeout after 1s,
+	// then Alice and Bob will try to reconnect.
+	time.Sleep(2 * time.Second)
+
+	// The above quiescence timeout will cause Alice to disconnect with Bob.
+	// However, since the connection has an open channel, Alice and Bob will
+	// be reconnected shortly.
+	//
+	// TODO(yy): When refactoring the peer connection we need to have an RPC
+	// that reports how many times the peer has been connected.
+	ht.AssertConnected(alice, bob)
+
+	// Assert that Alice can pay invoice3. This implicitly checks that the
+	// above quiescence is terminated.
+	req = &routerrpc.SendPaymentRequest{
+		PaymentRequest: invoice3.PaymentRequest,
+		FeeLimitMsat:   noFeeLimitMsat,
+	}
+	ht.SendPaymentAssertSettled(alice, req)
 }
